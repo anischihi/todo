937367cfeae71d2ac20eea0130a6eadd7d7d25d5
anischihi <anislchihi@gmail.com>
2020-02-23 16:19:15 +0100

    adds todo table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 // welcome
 const routes: Routes = [
+  {path:'todos', component: ListTodosComponent},
   {path:'', component: LoginComponent},
   {path:'welcome/:name', component: WelcomeComponent},
   {path:'login', component: LoginComponent},
